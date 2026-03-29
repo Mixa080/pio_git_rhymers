3d23c747fbe1fe92205ff96b893d10de884c7340
Mykhailo Shkurupii <smysyv@gmail.com>
2026-03-29 20:34:04 +0200

    12. Usuniecie nieuzywanych seterow

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -5,7 +5,6 @@ public class Node {
     private int value;
     private Node prev, next;
     public int getValue() { return value; }
-    public void setValue(int value) { this.value = value; }
 
     public Node getPrev() { return prev; }
     public void setPrev(Node prev) { this.prev = prev; }
