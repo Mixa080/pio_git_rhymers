69235fe5086972c301eb6bddd85b6c2d2868075b
Mykhailo Shkurupii <smysyv@gmail.com>
2026-03-30 11:47:05 +0200

    17 Wygenerowanie i uzupelnienie dokumentacji Javadoc w klasie IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Klasa reprezentujaca stos liczb calkowitych oparty na liscie wiazanej.
+ */
 public class IntLinkedList {
 
     private static final int EMPTY = -1;
@@ -8,6 +11,10 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     * Dodaje nowy element na szczyt stosu.
+     * @param i wartosc calkowita do dodania na stos
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -18,20 +25,38 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Sprawdza, czy stos jest pusty.
+     * @return true jesli stos jest pusty, w przeciwnym razie false
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Sprawdza, czy stos jest pelny.
+     * W tej implementacji stos oparty na liscie nigdy nie jest pelny.
+     * @return zawsze false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Zwraca wartosc elementu na szczycie stosu bez jego usuwania.
+     * @return wartosc na szczycie stosu lub wartosc EMPTY (-1), jesli stos jest pusty
+     */
     public int top() {
         if (isEmpty())
             return EMPTY;
         return last.getValue();
     }
 
+    /**
+     * Zwraca i usuwa element ze szczytu stosu.
+     * @return
+     * wartosc ze szczytu stosu lub wartosc EMPTY (-1), jesli stos jest pusty
+     */
     public int pop() {
         if (isEmpty())
             return EMPTY;
