07b66d542fb982f0383d29fe2085436397750a9b
Mykhailo Shkurupii <smysyv@gmail.com>
2026-03-30 11:33:32 +0200

    Poprawka do merge

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,7 +8,6 @@ private static final int STACK_CAPACITY = 12;
     private final int[] numbers = new int[STACK_CAPACITY];
 
     public int total = EMPTY;
-    private int[] numbers = new int[12];
 
 
     public int getTotal() {
