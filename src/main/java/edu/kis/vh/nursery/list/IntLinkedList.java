872f4fa02082d9e598a8d850c5c77a13beeeff6d
Mykhailo Shkurupii <smysyv@gmail.com>
2026-03-30 11:32:54 +0200

    14: Usuniecie publicznego modyfikatora z klasy Node i przeniesienie do IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -41,3 +41,19 @@ public class IntLinkedList {
     }
 
 }
+ class Node {
+
+    private int value;
+    private Node prev, next;
+    public int getValue() { return value; }
+
+    public Node getPrev() { return prev; }
+    public void setPrev(Node prev) { this.prev = prev; }
+
+    public Node getNext() { return next; }
+    public void setNext(Node next) { this.next = next; }
+    public Node(int i) {
+        value = i;
+    }
+
+}
